217b7713b47168b2e554bf54c4b9586466c642fa
Alistn14 <alis03tn@gmail.com>
2022-06-07 12:10:13 +0200

    Initial commit

---
+++ ejercicio9.ts
@@ -0,0 +1,15 @@
+const objeto1 : {
+    gestor: string;
+    cliente:string;
+    mensaje:string;
+    transferencia:string;
+} = {
+    gestor:"Banco CaixaBank",
+    cliente:"Alison",
+    mensaje:"Buenos días",
+    transferencia:"Realizada correctamente",
+}
+console.log(objeto1.mensaje)
+console.log(objeto1.gestor)
+
+
